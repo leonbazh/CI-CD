caf2d15457aefb1716f9a9d1ac2b33d1d982b5a3
kristieg <kristieg@student.21-school.ru>
2024-01-08 22:24:02 +0000

    3_part

--- src/style.sh
+++ src/style.sh
@@ -5,6 +5,7 @@ clang-format -n */*.c &> "style_check.txt"
 check=$(cat style_check.txt)
 if [[ -n $check ]];then
   echo Code is not clang-formatted
+  echo $check
   exit 1
 else
   echo Code is clang-formatted

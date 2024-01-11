b02e525e7cdf8f9adc4fa97ae86aecd756bb7d98
kristieg <kristieg@student.21-school.ru>
2024-01-11 13:57:41 +0000

    5_part

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 
-scp artifacts/s21_cat artifacts/s21_grep kristieg@172.24.116.8:/usr/bin/bash "echo suchkablyat"
-ssh kristieg@172.24.116.8 ls /usr/bin/bash
+scp artifacts/s21_cat artifacts/s21_grep kristieg@172.24.116.8:/usr/bin/bash

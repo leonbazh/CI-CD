f59efbd5ece3e88cd0de6f0cadc163ba524dd5fd
kristieg <kristieg@student.21-school.ru>
2024-01-11 15:24:53 +0000

    5_part

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 
-scp artifacts/s21_cat scp artifacts/s21_grep kristieg@172.24.116.8:.
+scp artifacts/s21_cat scp artifacts/s21_grep kristieg@172.24.116.8:/home/kristieg/
 ssh -p 22 kristieg@172.24.116.8
 mv s21_cat s21_grep /usr/local/bin

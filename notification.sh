d976cb0393cd3b170815a70c69ebc1e547d79972
kristieg <kristieg@student.21-school.ru>
2024-01-11 16:34:19 +0000

    6_part

--- notification.sh
+++ notification.sh
@@ -3,9 +3,10 @@
 TELEGRAM_BOT_TOKEN=6871042153:AAETVnXhOOlWpCs4ViIuUD6cXUHVSrQNgcU
 TELEGRAM_USER_ID=833274610
 TIME=15
+CI_PROJECT_NAME=DO6_CICD-1
 
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0Astage:+$CI_JOB_STAGE%0Astatus:+$CI_JOB_STATUS%0A"
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
 
